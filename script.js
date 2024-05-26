b029ec456592a3048ce9025ea4baa20b207572a8
Kicho115 <oscar.leonardo.ang@gmail.com>
2024-05-25 18:41:35 -0700

    Add the camera

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    75, // FOV
+    window.innerWidth/window.innerHeight, // Aspect
+    0.1, // Near
+    1000 // Far
+);
+
+// TODO: Cambiar la posicion de la camara cuando tengamops el laberinto
+camera.position.set(0,3,5);
